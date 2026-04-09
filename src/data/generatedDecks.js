d4c5cf2b824eda53b1617c9547b9113ce8cf7d65
stars228 <xc@stars228MacBook-Air.local>
2026-04-09 18:29:38 +0800

    feat: add Quareia deck

--- src/data/generatedDecks.js
+++ src/data/generatedDecks.js
@@ -1,8 +1,584 @@
 // ⚠️ 此文件由 import-deck.js 自动生成，请勿手动编辑
-// 最后生成时间: 2026/4/9 15:57:38
-// 牌组数量: 1
+// 最后生成时间: 2026/4/9 18:29:31
+// 牌组数量: 2
 
 export const GENERATED_DECKS = [
+  {
+    "id": "Quareia",
+    "name": "Quareia",
+    "nameCN": "Quareia",
+    "backImage": "/decks/Quareia/back.jpg",
+    "coverImage": "/decks/Quareia/bailiff.jpg",
+    "cards": [
+      {
+        "id": "Quareia-0",
+        "name": "bailiff",
+        "description": "bailiff - Quareia",
+        "image_url": "/decks/Quareia/bailiff.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-1",
+        "name": "blood ancestor",
+        "description": "blood ancestor - Quareia",
+        "image_url": "/decks/Quareia/blood-ancestor.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-2",
+        "name": "bridge of death",
+        "description": "bridge of death - Quareia",
+        "image_url": "/decks/Quareia/bridge-of-death.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-3",
+        "name": "challenge of the gods",
+        "description": "challenge of the gods - Quareia",
+        "image_url": "/decks/Quareia/challenge-of-the-gods.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-4",
+        "name": "chariot",
+        "description": "chariot - Quareia",
+        "image_url": "/decks/Quareia/chariot.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-5",
+        "name": "child",
+        "description": "child - Quareia",
+        "image_url": "/decks/Quareia/child.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-6",
+        "name": "communication",
+        "description": "communication - Quareia",
+        "image_url": "/decks/Quareia/communication.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-7",
+        "name": "death",
+        "description": "death - Quareia",
+        "image_url": "/decks/Quareia/death.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-8",
+        "name": "destruction",
+        "description": "destruction - Quareia",
+        "image_url": "/decks/Quareia/destruction.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-9",
+        "name": "disease",
+        "description": "disease - Quareia",
+        "image_url": "/decks/Quareia/disease.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-10",
+        "name": "elder",
+        "description": "elder - Quareia",
+        "image_url": "/decks/Quareia/elder.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-11",
+        "name": "faerie king",
+        "description": "faerie king - Quareia",
+        "image_url": "/decks/Quareia/faerie-king.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-12",
+        "name": "fate giver",
+        "description": "fate giver - Quareia",
+        "image_url": "/decks/Quareia/fate-giver.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-13",
+        "name": "fate holder",
+        "description": "fate holder - Quareia",
+        "image_url": "/decks/Quareia/fate-holder.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-14",
+        "name": "fate taker",
+        "description": "fate taker - Quareia",
+        "image_url": "/decks/Quareia/fate-taker.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-15",
+        "name": "fellowship",
+        "description": "fellowship - Quareia",
+        "image_url": "/decks/Quareia/fellowship.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-16",
+        "name": "female warrior",
+        "description": "female warrior - Quareia",
+        "image_url": "/decks/Quareia/female-warrior.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-17",
+        "name": "foundation  earth, north",
+        "description": "foundation  earth, north - Quareia",
+        "image_url": "/decks/Quareia/foundation_-earth,-north.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-18",
+        "name": "gate of the past",
+        "description": "gate of the past - Quareia",
+        "image_url": "/decks/Quareia/gate-of-the-past.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-19",
+        "name": "ghost",
+        "description": "ghost - Quareia",
+        "image_url": "/decks/Quareia/ghost.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-20",
+        "name": "giver of gifts",
+        "description": "giver of gifts - Quareia",
+        "image_url": "/decks/Quareia/giver-of-gifts.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-21",
+        "name": "glamour",
+        "description": "glamour - Quareia",
+        "image_url": "/decks/Quareia/glamour.jpg",
+        "isMajor": true
+      },
+      {
+        "id": "Quareia-22",
+        "name": "goblin queen",
+        "description": "goblin queen - Quareia",
+        "image_url": "/decks/Quareia/goblin-queen.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-23",
+        "name": "guardians of the inner desert",
+        "description": "guardians of the inner desert - Quareia",
+        "image_url": "/decks/Quareia/guardians-of-the-inner-desert.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-24",
+        "name": "healer",
+        "description": "healer - Quareia",
+        "image_url": "/decks/Quareia/healer.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-25",
+        "name": "hidden knowledge",
+        "description": "hidden knowledge - Quareia",
+        "image_url": "/decks/Quareia/hidden-knowledge.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-26",
+        "name": "hierophant",
+        "description": "hierophant - Quareia",
+        "image_url": "/decks/Quareia/hierophant.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-27",
+        "name": "home and hearth",
+        "description": "home and hearth - Quareia",
+        "image_url": "/decks/Quareia/home-and-hearth.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-28",
+        "name": "idiot",
+        "description": "idiot - Quareia",
+        "image_url": "/decks/Quareia/idiot.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-29",
+        "name": "inner sanctum",
+        "description": "inner sanctum - Quareia",
+        "image_url": "/decks/Quareia/inner-sanctum.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-30",
+        "name": "inspiration",
+        "description": "inspiration - Quareia",
+        "image_url": "/decks/Quareia/inspiration.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-31",
+        "name": "keeper of justice",
+        "description": "keeper of justice - Quareia",
+        "image_url": "/decks/Quareia/keeper-of-justice.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-32",
+        "name": "limiter  air, east",
+        "description": "limiter  air, east - Quareia",
+        "image_url": "/decks/Quareia/limiter_-air,-east.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-33",
+        "name": "luna",
+        "description": "luna - Quareia",
+        "image_url": "/decks/Quareia/luna.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-34",
+        "name": "magical attack",
+        "description": "magical attack - Quareia",
+        "image_url": "/decks/Quareia/magical-attack.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-35",
+        "name": "magical temple",
+        "description": "magical temple - Quareia",
+        "image_url": "/decks/Quareia/magical-temple.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-36",
+        "name": "male warrior",
+        "description": "male warrior - Quareia",
+        "image_url": "/decks/Quareia/male-warrior.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-37",
+        "name": "mother earth x",
+        "description": "mother earth x - Quareia",
+        "image_url": "/decks/Quareia/mother-earth-x.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-38",
+        "name": "occultist",
+        "description": "occultist - Quareia",
+        "image_url": "/decks/Quareia/occultist.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-39",
+        "name": "parasite",
+        "description": "parasite - Quareia",
+        "image_url": "/decks/Quareia/parasite.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-40",
+        "name": "path of hercules",
+        "description": "path of hercules - Quareia",
+        "image_url": "/decks/Quareia/path-of-hercules.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-41",
+        "name": "place of healing",
+        "description": "place of healing - Quareia",
+        "image_url": "/decks/Quareia/place-of-healing.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-42",
+        "name": "premonition",
+        "description": "premonition - Quareia",
+        "image_url": "/decks/Quareia/premonition.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-43",
+        "name": "priestess magician",
+        "description": "priestess magician - Quareia",
+        "image_url": "/decks/Quareia/priestess-magician.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-44",
+        "name": "pure balance vi",
+        "description": "pure balance vi - Quareia",
+        "image_url": "/decks/Quareia/pure-balance-vi.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-45",
+        "name": "regenerator  water, west",
+        "description": "regenerator  water, west - Quareia",
+        "image_url": "/decks/Quareia/regenerator_-water,-west.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-46",
+        "name": "resources",
+        "description": "resources - Quareia",
+        "image_url": "/decks/Quareia/resources.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-47",
+        "name": "river of dreams",
+        "description": "river of dreams - Quareia",
+        "image_url": "/decks/Quareia/river-of-dreams.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-48",
+        "name": "scapegoat",
+        "description": "scapegoat - Quareia",
+        "image_url": "/decks/Quareia/scapegoat.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-49",
+        "name": "seclusion",
+        "description": "seclusion - Quareia",
+        "image_url": "/decks/Quareia/seclusion.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-50",
+        "name": "shamaness",
+        "description": "shamaness - Quareia",
+        "image_url": "/decks/Quareia/shamaness.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-51",
+        "name": "sol",
+        "description": "sol - Quareia",
+        "image_url": "/decks/Quareia/sol.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-52",
+        "name": "spirit guide",
+        "description": "spirit guide - Quareia",
+        "image_url": "/decks/Quareia/spirit-guide.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-53",
+        "name": "staff of the gods  fire, south",
+        "description": "staff of the gods  fire, south - Quareia",
+        "image_url": "/decks/Quareia/staff-of-the-gods_-fire,-south.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-54",
+        "name": "temple of ancestors",
+        "description": "temple of ancestors - Quareia",
+        "image_url": "/decks/Quareia/temple-of-ancestors.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-55",
+        "name": "temptation",
+        "description": "temptation - Quareia",
+        "image_url": "/decks/Quareia/temptation.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-56",
+        "name": "the abyss",
+        "description": "the abyss - Quareia",
+        "image_url": "/decks/Quareia/the-abyss.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-57",
+        "name": "the archon and the aion",
+        "description": "the archon and the aion - Quareia",
+        "image_url": "/decks/Quareia/the-archon-and-the-aion.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-58",
+        "name": "the creator of time ii",
+        "description": "the creator of time ii - Quareia",
+        "image_url": "/decks/Quareia/the-creator-of-time-ii.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-59",
+        "name": "the grindstone vii",
+        "description": "the grindstone vii - Quareia",
+        "image_url": "/decks/Quareia/the-grindstone-vii.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-60",
+        "name": "the holder of light iii",
+        "description": "the holder of light iii - Quareia",
+        "image_url": "/decks/Quareia/the-holder-of-light-iii.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-61",
+        "name": "the imprisoner v",
+        "description": "the imprisoner v - Quareia",
+        "image_url": "/decks/Quareia/the-imprisoner-v.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-62",
+        "name": "the inner companion",
+        "description": "the inner companion - Quareia",
+        "image_url": "/decks/Quareia/the-inner-companion.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-63",
+        "name": "the inner librarian",
+        "description": "the inner librarian - Quareia",
+        "image_url": "/decks/Quareia/the-inner-librarian.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-64",
+        "name": "the inner temple",
+        "description": "the inner temple - Quareia",
+        "image_url": "/decks/Quareia/the-inner-temple.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-65",
+        "name": "the keeper of the abyss",
+        "description": "the keeper of the abyss - Quareia",
+        "image_url": "/decks/Quareia/the-keeper-of-the-abyss.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-66",
+        "name": "the leader",
+        "description": "the leader - Quareia",
+        "image_url": "/decks/Quareia/the-leader.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-67",
+        "name": "the light bearer iv",
+        "description": "the light bearer iv - Quareia",
+        "image_url": "/decks/Quareia/the-light-bearer-iv.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-68",
+        "name": "the man of nature",
+        "description": "the man of nature - Quareia",
+        "image_url": "/decks/Quareia/the-man-of-nature.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-69",
+        "name": "the mystic",
+        "description": "the mystic - Quareia",
+        "image_url": "/decks/Quareia/the-mystic.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-70",
+        "name": "the oracle",
+        "description": "the oracle - Quareia",
+        "image_url": "/decks/Quareia/the-oracle.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-71",
+        "name": "the protector of souls",
+        "description": "the protector of souls - Quareia",
+        "image_url": "/decks/Quareia/the-protector-of-souls.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-72",
+        "name": "the star father i",
+        "description": "the star father i - Quareia",
+        "image_url": "/decks/Quareia/the-star-father-i.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-73",
+        "name": "the unraveller viii",
+        "description": "the unraveller viii - Quareia",
+        "image_url": "/decks/Quareia/the-unraveller-viii.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-74",
+        "name": "the utterer",
+        "description": "the utterer - Quareia",
+        "image_url": "/decks/Quareia/the-utterer.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-75",
+        "name": "the weaver of creation",
+        "description": "the weaver of creation - Quareia",
+        "image_url": "/decks/Quareia/the-weaver-of-creation.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-76",
+        "name": "threshold guardian ix",
+        "description": "threshold guardian ix - Quareia",
+        "image_url": "/decks/Quareia/threshold-guardian-ix.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-77",
+        "name": "underworld forest",
+        "description": "underworld forest - Quareia",
+        "image_url": "/decks/Quareia/underworld-forest.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-78",
+        "name": "union",
+        "description": "union - Quareia",
+        "image_url": "/decks/Quareia/union.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-79",
+        "name": "wheel of fate",
+        "description": "wheel of fate - Quareia",
+        "image_url": "/decks/Quareia/wheel-of-fate.jpg",
+        "isMajor": false
+      },
+      {
+        "id": "Quareia-80",
+        "name": "wise teacher",
+        "description": "wise teacher - Quareia",
+        "image_url": "/decks/Quareia/wise-teacher.jpg",
+        "isMajor": false
+      }
+    ]
+  },
   {
     "id": "mystagogus",
     "name": "mystagogus",
